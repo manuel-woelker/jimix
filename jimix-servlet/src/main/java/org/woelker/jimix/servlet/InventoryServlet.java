7e5783bea1fd919b79a5ba8d4d8fe8c92f61a6a4
Manuel Woelker <github@manuel.woelker.org>
2014-04-10 22:16:50 +0200

    servlet: url dispatching logic

---
+++ jimix-servlet/src/main/java/org/woelker/jimix/servlet/InventoryServlet.java
@@ -0,0 +1,59 @@
+package org.woelker.jimix.servlet;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanInfo;
+import javax.management.MBeanServer;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InventoryServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
+
+        Set<ObjectInstance> instances = mbeanServer.queryMBeans(null, null);
+        Map<String, Object> result = new HashMap<String, Object>();
+        final ArrayList<Object> mbeanList = new ArrayList<Object>();
+        result.put("mbeans", mbeanList);
+        Iterator<ObjectInstance> iterator = instances.iterator();
+        resp.setContentType("application/json");
+        while (iterator.hasNext()) {
+            try {
+                ObjectInstance instance = iterator.next();
+                final HashMap<String, Object> mbean = new HashMap<String, Object>();
+                final ObjectName objectName = instance.getObjectName();
+                final MBeanInfo mBeanInfo = mbeanServer.getMBeanInfo(objectName);
+                final String description = mBeanInfo.getDescription();
+                mbean.put("objectName", objectName.getCanonicalName());
+                mbean.put("description", description);
+                final ArrayList<Object> attributes = new ArrayList<Object>();
+                mbean.put("attributes", attributes);
+                mbeanList.add(mbean);
+                for (MBeanAttributeInfo attributeInfo : mBeanInfo.getAttributes()) {
+                    final HashMap<String, Object> attribute = new HashMap<String, Object>();
+                    attribute.put("name", attributeInfo.getName());
+                    attribute.put("type", attributeInfo.getType());
+                    attribute.put("description", attributeInfo.getDescription());
+                    attributes.add(attribute);
+                }
+            } catch (Throwable throwable) {
+                // TODO: how to handle?
+            }
+
+        }
+        new JsonWriter(resp.getOutputStream()).serialize(result);
+    }
+
+}
--- jimix-servlet/src/main/java/org/woelker/jimix/servlet/JimixServlet.java
+++ jimix-servlet/src/main/java/org/woelker/jimix/servlet/JimixServlet.java
@@ -1,26 +1,11 @@
 package org.woelker.jimix.servlet;
 
-import java.io.ByteArrayOutputStream;
+import com.sun.jmx.remote.util.OrderClassLoaders;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.management.InstanceNotFoundException;
-import javax.management.IntrospectionException;
-import javax.management.MBeanAttributeInfo;
-import javax.management.MBeanInfo;
-import javax.management.MBeanServer;
-import javax.management.ObjectInstance;
-import javax.management.ObjectName;
-import javax.management.ReflectionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,83 +13,46 @@ import javax.servlet.http.HttpServletResponse;
 
 public class JimixServlet extends HttpServlet {
 
-    Map<String, String> mimeTypeMap = new HashMap<String, String>();
+    public static class DispatchEntry {
 
-    {
-        mimeTypeMap.put("html", "text/html");
-        mimeTypeMap.put("css", "text/css");
-        mimeTypeMap.put("js", "application/javascript");
+        Pattern pattern;
+        HttpServlet servlet;
+
+        public DispatchEntry(Pattern pattern, HttpServlet servlet) {
+            this.pattern = pattern;
+            this.servlet = servlet;
+        }
+
+    }
+    ArrayList<DispatchEntry> dispatchList = new ArrayList<DispatchEntry>();
+
+    public JimixServlet() {
+        addDispatch("/api/inventory", new InventoryServlet());
+        addDispatch("/api/mbeans/([^/]+)", new InventoryServlet());
+        addDispatch(".*", new AssetServlet());
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getPathInfo() == null) {
             resp.sendRedirect(req.getRequestURI() + "/");
             return;
         }
         String path = req.getPathInfo();
-        if (path.matches("/api/inventory")) {
-            getInventory(req, resp);
-            return;
-        }
-        if ("/".equals(path)) {
-            path = "/index.html";
-        }
-        final InputStream inputStream = JimixServlet.class.getResourceAsStream("../ui/assets" + path);
-        if (inputStream == null) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-        try {
-            final int lastDotIndex = path.lastIndexOf(".");
-            final String extension = path.substring(lastDotIndex + 1);
-            String mimeType = mimeTypeMap.get(extension);
-            if (mimeType == null) {
-                mimeType = "text/plain";
-            }
-            resp.setContentType(mimeType);
-            JimixIOUtils.copy(inputStream, resp.getOutputStream());
-        } finally {
-            inputStream.close();
-        }
-    }
-
-    private void getInventory(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        MBeanServer mbeanServer = ManagementFactory.getPlatformMBeanServer();
-
-        Set<ObjectInstance> instances = mbeanServer.queryMBeans(null, null);
-        Map<String, Object> result = new HashMap<String, Object>();
-        final ArrayList<Object> mbeanList = new ArrayList<Object>();
-        result.put("mbeans", mbeanList);
-        Iterator<ObjectInstance> iterator = instances.iterator();
-        resp.setContentType("application/json");
-        while (iterator.hasNext()) {
-            try {
-                ObjectInstance instance = iterator.next();
-                final HashMap<String, Object> mbean = new HashMap<String, Object>();
-                final ObjectName objectName = instance.getObjectName();
-                final MBeanInfo mBeanInfo = mbeanServer.getMBeanInfo(objectName);
-                final String description = mBeanInfo.getDescription();
-                mbean.put("objectName", objectName.getCanonicalName());
-                mbean.put("description", description);
-                final ArrayList<Object> attributes = new ArrayList<Object>();
-                mbean.put("attributes", attributes);
-                mbeanList.add(mbean);
-                for (MBeanAttributeInfo attributeInfo : mBeanInfo.getAttributes()) {
-                    final HashMap<String, Object> attribute = new HashMap<String, Object>();
-                    attribute.put("name", attributeInfo.getName());
-                    attribute.put("type", attributeInfo.getType());
-                    attribute.put("description", attributeInfo.getDescription());
-                    attributes.add(attribute);
+        for (DispatchEntry dispatchEntry : dispatchList) {
+            final Matcher matcher = dispatchEntry.pattern.matcher(path);            
+            if (matcher.matches()) {
+                for(int i = 0; i < matcher.groupCount(); i++) {
+                    req.setAttribute("param-"+i, matcher.group(i+1));
                 }
-            } catch (Throwable throwable) {
-                // TODO: how to handle?
+                dispatchEntry.servlet.service(req, resp);
+                return;
             }
-
         }
-        new JsonWriter(resp.getOutputStream()).serialize(result);
     }
-    
-    
+
+    private void addDispatch(String regex, HttpServlet servlet) {
+        dispatchList.add(new DispatchEntry(Pattern.compile("^" + regex+ "$"), servlet));
+    }
 
 }
