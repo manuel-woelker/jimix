b63da1846f778bcc9760144f38432e06efe7947e
Manuel Woelker <github@manuel.woelker.org>
2015-05-22 20:48:47 +0200

    feat(UI): basic inventory loading via superagent

--- ui/src/mbeans/list/MbeanListContainer.js
+++ ui/src/mbeans/list/MbeanListContainer.js
@@ -1,17 +1,33 @@
 import React from "react";
+import request from 'superagent';
 
-import {Input, Button, Glyphicon, Panel} from "react-bootstrap";
+import {Input, Button, Glyphicon, Panel, Well} from "react-bootstrap";
 
 export default React.createClass({
+  getInitialState() {
+    request
+      .get('/jimix/api/inventory')
+      .end((err, res) => {
+        this.setState(res.body);
+      });
+    return {
+      mbeans: []
+    };
+  },
+
   render: function () {
     return (
       <div>
         <form>
           <Input type='text' addonAfter={<Glyphicon glyph='remove' />}/>
         </form>
-        <Panel header={<h4>Header</h4>}>
-          Panel content
-        </Panel>
+        <Well bsSize='small' style={{overflow: "hidden"}}>
+          <bold style={{fontWeight: "bold"}}>Header</bold>
+          {this.state.mbeans.map(mbean => {
+            return <div key={mbean.objectName}>{mbean.objectName}</div>;
+          })}
+          {/*JSON.stringify(this.state.mbeans)*/}
+        </Well>
 
       </div>
     );
