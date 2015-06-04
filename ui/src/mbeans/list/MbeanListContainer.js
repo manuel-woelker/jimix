883efd46f2c343d8c28ffc5b8975e6c39f3627e5
Manuel Woelker <github@manuel.woelker.org>
2015-06-04 07:58:45 +0200

    feat(UI/list): mbean list

--- ui/src/mbeans/list/MbeanListContainer.js
+++ ui/src/mbeans/list/MbeanListContainer.js
@@ -3,32 +3,46 @@ import request from 'superagent';
 import Reflux from 'reflux';
 import InventoryStore from '../../inventory/InventoryStore.js';
 
-import {Input, Button, Glyphicon, Panel, Well} from "react-bootstrap";
+import {Input, Button, Glyphicon, Panel, Well, ListGroup, ListGroupItem, Table} from "react-bootstrap";
 
 export default React.createClass({
-  mixins: [Reflux.connect(InventoryStore,"inventory")],
+	mixins: [Reflux.connect(InventoryStore, "inventory")],
 
-  render: function () {
-    let inventory = this.state.inventory;
-    if(!inventory) {
-      return null;
-    }
-    return (
-      <div>
-        <form>
-          <Input type='text' addonAfter={<Glyphicon glyph='remove' />}/>
-        </form>
-        {inventory.domains.map(domain => {
-          return <Well className="domain" key={domain.name} bsSize='small' style={{overflow: "hidden"}}>
-          <bold style={{fontWeight: "bold"}}>{domain.name}</bold>
-          {domain.mbeans.map(mbean => {
-            return <div className="mbean" key={mbean.objectName}>{mbean.objectName}</div>;
-          })}
-          {/*JSON.stringify(this.state.mbeans)*/}
-        </Well>;
-        })}
+	render: function () {
+		let inventory = this.state.inventory;
+		if (!inventory) {
+			return null;
+		}
+		return (
+			<div>
+				<form>
+					<Input type='text' addonAfter={<Glyphicon glyph='remove' />}/>
+				</form>
+				{inventory.domains.map(domain => {
+					return <Panel key={domain.name} style={{overflow: "hidden"}}
+								  header={<bold style={{fontWeight: "bold"}}>{domain.name}</bold>}>
+						<Table striped condensed hover fill>
+							<tbody>
+							{domain.mbeans.map(mbean => {
+								return <tr key={mbean.objectName}><td>{mbean.name}</td></tr>;
+							})}
+							</tbody>
+						</Table>
+						{/*
+						<ListGroup fill>
+							<ListGroupItem>Item 1</ListGroupItem>
+							<ListGroupItem>Item 2</ListGroupItem>
+							<ListGroupItem>&hellip;</ListGroupItem>
+						</ListGroup>
+						{domain.mbeans.map(mbean => {
+							return <div className="mbean" key={mbean.objectName}>{mbean.objectName}</div>;
+						})}
+						 */}
+						{/*JSON.stringify(this.state.mbeans)*/}
+					</Panel>;
+				})}
 
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 });
